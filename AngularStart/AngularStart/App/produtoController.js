130619679809b20c756145c7bbc38720a1296371
RodrigoNeuber <rodrigoneuber@outlook.com>
2016-06-13 13:47:35 -0300

    Correção de um a erro encontrado

--- AngularStart/AngularStart/App/produtoController.js
+++ AngularStart/AngularStart/App/produtoController.js
@@ -36,28 +36,4 @@
         console.error(erro);
     });
 
-    $http.get('http://localhost:52368/api/Software/1')
-        .success(function (software1) {
-            $scope.software1 = software1;
-        })
-    .error(function (erro) {
-        console.error(erro);
-    });
-
-    $http.get('http://localhost:52368/api/Software/2')
-        .success(function (software2) {
-            $scope.software2 = software2;
-        })
-    .error(function (erro) {
-        console.error(erro);
-    });
-
-    $http.get('http://localhost:52368/api/Software/3')
-        .success(function (software3) {
-            $scope.software3 = software3;
-        })
-    .error(function (erro) {
-        console.error(erro);
-    });
-
 });
